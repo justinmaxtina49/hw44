62dcf3893a2327b7f2fa61dde979d9a7745d04ca
WeiMing <justinmaxtina49@gmail.com>
2020-05-03 21:35:51 +0800

    Initial Commit

---
+++ hw44/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw44
+//
+//  Created by WeiMing on 2020/5/3.
+//  Copyright © 2020 WeiMing. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
